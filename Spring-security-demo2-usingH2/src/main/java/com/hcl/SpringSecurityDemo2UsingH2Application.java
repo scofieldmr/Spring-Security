41d1d54c2847a10d735f5f83d663594fbedbfdbe
Mohamed Bilal <bilalumar95@gmail.com>
2024-10-21 20:57:09 +0530

    Spring Security with H2 Database

---
+++ Spring-security-demo2-usingH2/src/main/java/com/hcl/SpringSecurityDemo2UsingH2Application.java
@@ -0,0 +1,13 @@
+package com.hcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemo2UsingH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDemo2UsingH2Application.class, args);
+	}
+
+}
---
+++ Spring-security-demo2-usingH2/src/test/java/com/hcl/SpringSecurityDemo2UsingH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemo2UsingH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
