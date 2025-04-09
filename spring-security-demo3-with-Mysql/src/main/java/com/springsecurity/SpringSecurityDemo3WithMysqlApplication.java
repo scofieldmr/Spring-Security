5b4a23de24b4a3e0fa646a1effb60db6a8d51684
Mohamed Bilal <bilalumar95@gmail.com>
2025-04-09 06:29:31 +0530

    Spring security - User with Roles using MySQL

---
+++ spring-security-demo3-with-Mysql/src/main/java/com/springsecurity/SpringSecurityDemo3WithMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemo3WithMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityDemo3WithMysqlApplication.class, args);
+    }
+
+}
---
+++ spring-security-demo3-with-Mysql/src/test/java/com/springsecurity/SpringSecurityDemo3WithMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemo3WithMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
